ae31d725fa0997f3c6e85005ee5d762440a7f69f
demet <demetratzeka@gmail.com>
2023-05-26 22:30:51 +0300

    Method fetchJobs completed.

--- src/main/java/com/carops/JobCatalog.java
+++ src/main/java/com/carops/JobCatalog.java
@@ -3,10 +3,8 @@ package com.carops;
 import java.util.ArrayList;
 
 public class JobCatalog {
-	private ArrayList<Job> jobs = new ArrayList<Job>();
+	private static ArrayList<Job> jobs = new ArrayList<Job>();
 	public static ArrayList<Job> fetchJobs() {
-		// TODO - implement JobCatalog.fetchJobs
-		throw new UnsupportedOperationException();
+		return jobs;
 	}
-
 }
