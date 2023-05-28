243ae2c6bd242b1b238b53f7e39808a20ac14b3e
demet <demetratzeka@gmail.com>
2023-05-28 15:42:52 +0300

    Προσθήκη απαραίτητων μεθόδων στο JobCatalog και VehicleCatalog.

--- src/main/java/com/carops/JobCatalog.java
+++ src/main/java/com/carops/JobCatalog.java
@@ -3,10 +3,31 @@ package com.carops;
 import java.util.ArrayList;
 
 public class JobCatalog {
-	private ArrayList<Job> jobs = new ArrayList<Job>();
+	private static ArrayList<Job> jobs = new ArrayList<Job>();
 	public static ArrayList<Job> fetchJobs() {
-		// TODO - implement JobCatalog.fetchJobs
-		throw new UnsupportedOperationException();
+		return jobs;
+	}
+	public static void addJob(Job job){
+		jobs.add(job);
 	}
 
+	public static void printData(){
+		System.out.printf("\n> All Jobs in Catalog:\nn %-25s  %-10s\n", "Name", "Price");
+		int i=1;
+		for (Job job : jobs) {
+			System.out.printf("%d|",i);
+			job.printData();
+			i++;
+		}
+	}
+
+	public static Job findJobByName(String jobName){
+		for ( Job job : jobs) {
+			if (job.getName().equalsIgnoreCase(jobName))
+			{
+				return  job;
+			}
+		}
+		return null;
+	}
 }
