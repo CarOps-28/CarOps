6e3555fb74c11c558b174fa3873417f7315e9878
EriolaH <erjolahajdari03@gmail.com>
2023-05-28 15:30:54 +0300

    ArrayLists for appointment and customer catalogs changed to  static

--- src/main/java/com/carops/AppointmentCatalog.java
+++ src/main/java/com/carops/AppointmentCatalog.java
@@ -5,18 +5,12 @@ import java.util.ArrayList;
 
 public class AppointmentCatalog {
 
-	private ArrayList<Appointment> appointments = new ArrayList<Appointment>();
+	private static ArrayList<Appointment> appointments = new ArrayList<Appointment>();
 	public static ArrayList<Appointment> fetchAppointments() {
-		// TODO - implement AppointmentCatalog.fetchAppointments
-		//Print appointments     μαλλον για την μειν?
-		//for (Appointment appointment : fetchedAppointments) {
-			//System.out.println("Ημερομηνία Ραντεβού: " + appointment.getDate() + ", Πινακίδα: " + appointment.getVehiclePlateNumber() + ", Ώρα Ραντεβού¨: " + appointment.getDate().getTime() + ", Κατάσταση¨: " + appointment.getRepairfile().getStatus());
-		//}
 		return appointments;
 	}
 
 	public static void addAppointment(Appointment appointment) {
-		// TODO - implement AppointmentCatalog.addAppointment
 		appointments.add(appointment);
 	}
 
--- src/main/java/com/carops/CustomerCatalog.java
+++ src/main/java/com/carops/CustomerCatalog.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 
 	public class CustomerCatalog {
 
-	private ArrayList<Customer> customers = new ArrayList<Customer>();
+	private static ArrayList<Customer> customers = new ArrayList<Customer>();
 	public static Customer fetchCustomerById(String customerId) {
-		// TODO - implement CustomerCatalog.fetchCustomerById
 		for (Customer customer : customers) {
-			if (customer.getId() == customerId) {    // δεν ειμαι σιγουρη αν το customer.getId() ειναι σωστό η αν θελει κατι με super()
+			if (customer.getId() == customerId) {
 				return customer;
 			}
 		}
@@ -16,7 +15,6 @@ import java.util.ArrayList;
 	}
 
 	public static Customer fetchCustomerByPhoneNumber(String phoneNumber) {
-		// TODO - implement CustomerCatalog.fetchCustomerByPhoneNumber
 		for (Customer customer : customers) {
 			if (customer.getPhoneNumber() == phoneNumber) {
 				return customer;
@@ -26,7 +24,6 @@ import java.util.ArrayList;
 	}
 
 	public static void addCustomer(Customer customer) {
-		// TODO - implement CustomerCatalog.addCustomer
 		customers.add(customer);
 	}
 
